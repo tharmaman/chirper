8497abe01efa6401dd351186b4037b569499b1c6
tharmaman <daniel.tharmathurai@mail.mcgill.ca>
2018-09-17 16:24:31 -0400

    enabled logout and login features

--- chirper-client/src/containers/Navbar.js
+++ chirper-client/src/containers/Navbar.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import { logout } from "../store/actions/auth";
 import Logo from "../images/warbler-logo.png"
 
 class Navbar extends Component {
+    logout = event => {
+        event.preventDefault();
+        this.props.logout();
+    }
     render() {
         return (
             <nav className="navbar navbar-expand">
@@ -13,6 +18,20 @@ class Navbar extends Component {
                             <img src={Logo} alt="Chirper Home" />
                         </Link>
                     </div>
+                    {this.props.currentUser.isAuthenticated ? (
+                        <ul className="nav-navbar-nav navbar-right">
+                            <li>
+                                <Link to={`/users/${this.props.currentUser.user.id}/messages/new`}> New Message 
+                                </Link>
+                            </li>
+                            <li>
+                                <a onClick={this.logout}>
+                                    Log Out 
+                                </a> 
+                            </li>
+                        </ul>
+                    )
+                    : (
                     <ul className="nav navbar-nav navbar-right">
                         <li>
                             <Link to="/signup">Sign Up</Link>
@@ -21,6 +40,7 @@ class Navbar extends Component {
                             <Link to="/signin">Log In</Link>
                         </li>
                     </ul>
+                    )}
                 </div>
             </nav>
         );
@@ -33,4 +53,4 @@ function mapStateToProps(state) {
     };
 }
 
-export default connect(mapStateToProps, null)(Navbar);
+export default connect(mapStateToProps, {logout})(Navbar);
