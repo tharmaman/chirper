b0f97464dee02034241aa5ab5bffb6469f46c1c7
tharmaman <daniel.tharmathurai@mail.mcgill.ca>
2018-09-18 19:46:55 -0400

    added process env

--- chirper-server/models/index.js
+++ chirper-server/models/index.js
@@ -1,7 +1,9 @@
+require("dotenv").load();
+
 const mongoose = require("mongoose");
 mongoose.set("debug", true);
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/chirper", {
+mongoose.connect(process.env.MLAB_URI || "mongodb://localhost/chirper", {
     keepAlive: true,
     useNewUrlParser: true
 });
