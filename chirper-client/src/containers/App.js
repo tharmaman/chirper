22334c15e4271d72ac127cfcfd7e7838e9cf4d81
tharmaman <daniel.tharmathurai@mail.mcgill.ca>
2018-09-17 20:41:48 -0400

    Fixed messages loading on site

--- chirper-client/src/containers/App.js
+++ chirper-client/src/containers/App.js
@@ -4,8 +4,24 @@ import { Provider } from "react-redux";
 import { configureStore } from "../store";
 import Navbar from "./Navbar";
 import Main from "./Main";
+import { setAuthorizationToken, setCurrentUser } from '../store/actions/auth';
+import jwtDecode from "jwt-decode";
+
+// idea of hydration => if server goes down, when page refreshes we can see if there's a token
 
 const store = configureStore();
+
+if(localStorage.jwtToken){
+  setAuthorizationToken(localStorage.jwtToken)
+  // prevent someone from manually tampering with the key of jwtToken in localStorage
+  // e.g. clear the redux store
+  try {
+    store.dispatch(setCurrentUser(jwtDecode(localStorage.jwtToken)));
+  // if they try to modify then force current user to logout
+  } catch(e) {
+    store.dispatch(setCurrentUser({}));
+  }
+}
 
 const App = () => (
   <Provider store={store}>
